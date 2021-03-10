6719127ab1053b0a30a5485ece44a63d93d46f8a
Vadim <vadim.mozeiko@gmail.com>
2021-03-10 16:24:22 +0200

    working model

--- src/cars/CARS.java
+++ src/cars/CARS.java
@@ -10,21 +10,25 @@ public class CARS {
         System.out.println();
         System.out.println("-------- Car Race Game -------- ");
         System.out.println();
+        System.out.println("Enter number of cars:");
         System.out.println();
-        int cars[] = new int[8];
+        int numberOfCars = sc.nextInt();
+        System.out.println();
+        System.out.println("Enter distance: ");
+        int maxDistance = sc.nextInt();
+        System.out.println();
 
-        for (int i = 0; i < cars.length; i++) {
-            for (int k = 0; k < cars.length; k++) {
-                for (int j = 0; j < cars.length; j++) {
-                    if (cars[j] < 100) {
-                        cars[j] += ((Math.random()) * 5) + 1;
-                    } else {
-                        break;
-                    }
+        int cars[] = new int[numberOfCars];
+        int distance = 0;
+
+        do {
+            for (int i = 0; i < cars.length; i++) {
+                cars[i] += (int) (Math.random() * 5 + 1);
+                if (distance < cars[i]) {
+                    distance = cars[i];
                 }
             }
-        }
-
+        } while (distance < maxDistance);
         for (int i = 0; i < cars.length; i++) {
             System.out.println((i + 1) + " car distance is: " + cars[i]);
             System.out.println();
