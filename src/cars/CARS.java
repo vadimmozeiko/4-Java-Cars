c12dee9941d937ccb543efeaf8998c6f29569a71
Vadim <vadim.mozeiko@gmail.com>
2021-03-10 14:44:41 +0200

    kind of works

--- src/cars/CARS.java
+++ src/cars/CARS.java
@@ -1,22 +1,41 @@
-
 package cars;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class CARS {
 
     public static void main(String[] args) {
-    
         
-        int cars[] = new int[8];
-        
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------- Car Race Game -------- ");
+        System.out.println();
+        System.out.println("Choose how many cars: ");
+        System.out.println();
+        int numberOfCars = sc.nextInt();
+        System.out.println();
+
+        int cars[] = new int[numberOfCars];
+
         for (int i = 0; i < cars.length; i++) {
-            if (cars[i] < 100){
-               cars[i] += ((Math.random())*5)+1;
-                System.out.println(Arrays.toString(cars));
+            if (cars[i] >= 100) {
+                break;
+            } else {
+                for (int k = 0; k < cars.length; k++) {
+                    for (int j = 0; j < cars.length; j++) {
+                        if (cars[j] < 100) {
+                            cars[j] += ((Math.random()) * 5) + 1;
+                        } else {
+                            break;
+                        }
+                    }
+                }
             }
         }
+
+        for (int z = 0; z < cars.length; z++) {
+            System.out.println((z + 1) + " car distance is: " + cars[z]);
+            System.out.println();
+        }
+        System.out.println();
     }
-    
 }
